e1f53413075b901343dc7a88c53ed811b1617205
Fay-chebby <chebetf034@gmail.com>
2025-04-10 23:52:16 +0300

    done

--- Book.sql
+++ Book.sql
@@ -1,7 +1,117 @@
 CREATE DATABASE Bookstore;
 
 USE Bookstore;
+ -- Order_status table
+ CREATE TABLE Order_status(
+ status_id INT PRIMARY KEY AUTO_INCREMENT,
+ status_name VARCHAR(255) NOT NULL
+ );
 
+-- Shipping_method table
+ CREATE TABLE Shipping_method(
+ shipping_method_id INT PRIMARY KEY AUTO_INCREMENT,
+ method_name VARCHAR(255) NOT NULL
+ );
+ 
+ --Address_status table
+ CREATE TABLE Address_status(
+ status_id INT PRIMARY KEY AUTO_INCREMENT,
+ status_name VARCHAR(255) NOT NULL
+ );
+
+--Book_language table
+ CREATE TABLE Book_language(
+ language_id INT PRIMARY KEY AUTO_INCREMENT,
+ language_name VARCHAR (255) NOT NULL
+ );
+
+ --Publisher table
+ CREATE TABLE Publisher(
+publisher_id INT PRIMARY KEY AUTO_INCREMENT,
+publisher_name VARCHAR(255) NOT NULL
+);
+
+--Customer table
+CREATE TABLE Customer(
+customer_id INT PRIMARY KEY AUTO_INCREMENT,
+first_name VARCHAR(255) NOT NULL,
+last_name VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL,
+phone INT
+
+);
+
+--Country table
+CREATE TABLE Country(
+country_id INT PRIMARY KEY AUTO_INCREMENT,
+country_name VARCHAR(255) NOT NULL
+);
+
+--Address table
+CREATE TABLE Address (
+  address_id INT PRIMARY KEY AUTO_INCREMENT,
+  street VARCHAR(255) NOT NULL,
+  country_name VARCHAR(255) NOT NULL,
+  postal_code VARCHAR(255) NOT NULL,
+  country_id INT,  
+
+  FOREIGN KEY (country_id) REFERENCES Country(country_id)
+);
+
+-- Customer_address table
+  CREATE TABLE Customer_address (
+ customer_id INT PRIMARY KEY AUTO_INCREMENT,
+ address_id INT NOT NULL,
+
+ FOREIGN KEY (address_id) REFERENCES Address(address_id)
+);
+
+-- Customer_order table
+ CREATE TABLE Customer_order(
+ order_id INT PRIMARY KEY AUTO_INCREMENT,
+ customer_id INT,
+ order_date DATE,
+ shipping_method_id INT,
+ status_id INT,
+   
+   FOREIGN KEY (customer_id) REFERENCES Customer_address(customer_id),
+   FOREIGN KEY (shipping_method_id) REFERENCES Shipping_method(shipping_method_id),
+   FOREIGN KEY (status_id) REFERENCES address_status(status_id)
+   
+ );
+
+
+--Author table
+  CREATE TABLE Author(
+ author_id INT PRIMARY KEY AUTO_INCREMENT,
+ author_name VARCHAR(255) NOT NULL,
+ author_info VARCHAR(255) NOT NULL
+ );
+ 
+--Book_author table
+ CREATE  TABLE Book_author(
+ book_id INT PRIMARY KEY AUTO_INCREMENT,
+ author_id INT,
+  
+  
+FOREIGN KEY (author_id) REFERENCES Author(author_id),
+ FOREIGN KEY (book_id) REFERENCES book(book_id)
+
+ );
+
+
+-- Order_history table
+CREATE TABLE Order_history(
+history_id INT PRIMARY KEY AUTO_INCREMENT,
+order_id INT,
+status_id INT,
+change_date DATE,
+
+ FOREIGN KEY (order_id) REFERENCES Customer_order(order_id),
+ FOREIGN KEY (status_id) REFERENCES Address_status(status_id)
+);
+
+-- book table
 CREATE TABLE book (
     book_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
@@ -11,4 +121,17 @@ CREATE TABLE book (
 
     FOREIGN KEY (publisher_id) REFERENCES publisher(publisher_id),
     FOREIGN KEY (language_id) REFERENCES book_language(language_id)
+);
+
+-- order_line table
+ CREATE TABLE order_line(
+order_line_id INT PRIMARY KEY AUTO_INCREMENT,
+ order_id INT,
+book_id INT,
+quantity VARCHAR(255) NOT NULL,
+price VARCHAR(255) NOT NULL,
+
+FOREIGN KEY (order_id) REFERENCES Customer_order(order_id),
+FOREIGN KEY (book_id) REFERENCES book(book_id)
+
 );
